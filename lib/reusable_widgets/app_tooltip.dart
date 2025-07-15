c88158170f591778cfd9050ae9d7118787e00bba
KanawadeSakshii <sakshikanawade2@gmail.com>
2025-07-15 09:13:10 +0530

    app tooltip

---
+++ lib/reusable_widgets/app_tooltip.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppTooltip extends StatelessWidget {
+  final Widget child;
+  final String message;
+  final TextStyle? textStyle;
+  final Color? backgroundColor;
+  final EdgeInsetsGeometry? padding;
+  final double? verticalOffset;
+  final bool? preferBelow;
+
+  const AppTooltip({
+    super.key,
+    required this.child,
+    required this.message,
+    this.textStyle,
+    this.backgroundColor,
+    this.padding,
+    this.verticalOffset,
+    this.preferBelow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: message,
+      textStyle: textStyle,
+      decoration: BoxDecoration(
+        color: backgroundColor ?? Colors.black.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      padding:
+          padding ?? const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      verticalOffset: verticalOffset ?? 16,
+      preferBelow: preferBelow ?? true,
+      child: child,
+    );
+  }
+}
